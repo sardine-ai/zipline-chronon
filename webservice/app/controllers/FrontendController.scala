92662468b4f74edf58bd467afce665fdab3b122d
Piyush Narang <piyush@zipline.ai>
2024-09-23 11:49:36 -0400

    Fix scalafmt

--- webservice/app/controllers/FrontendController.scala
+++ webservice/app/controllers/FrontendController.scala
@@ -4,8 +4,9 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class FrontendController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
-  def home(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("Welcome to the Zipline homepage!"))
-  }
+class FrontendController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
+  def home(): Action[AnyContent] =
+    Action { implicit request: Request[AnyContent] =>
+      Ok(views.html.index("Welcome to the Zipline homepage!"))
+    }
 }
