d165191398a957fcfd8983fc79036920e2fdbdd3
chewy-zlai <chewys@zipline.ai>
2024-10-03 12:00:09 -0700

    Join and Group Bys for POC (#30)
    ## Summary
    Creates a join and a few group_bys for the Proof of Concept project.
     
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced aggregation functionalities for user purchases, transaction
    events, and merchant data.
    - Introduced new grouping operations for user and merchant transactions,
    allowing for detailed time-based analysis.
    - Configured structured joins to facilitate comprehensive risk
    management analysis across various datasets.
    
    - **Documentation**
    - Added a new "risk" section in the JSON structure for clarity on its
    purpose and namespace.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Chewy Shaw <chewys1024@gmail.com>

---
+++ api/py/test/sample/group_bys/risk/merchant_data.py
@@ -0,0 +1,29 @@
+from ai.chronon.api.ttypes import Source, EntitySource
+from ai.chronon.query import Query, select
+from ai.chronon.group_by import (
+    GroupBy,
+    Aggregation,
+    Operation,
+    Window,
+    TimeUnit
+)
+
+"""
+This GroupBy aggregates metrics about a user's previous purchases in various windows.
+"""
+
+# This source is raw purchase events. Every time a user makes a purchase, it will be one entry in this source.
+source_merchants = Source(
+    entities=EntitySource(
+        snapshotTable="data.merchants", # This points to the log table in the warehouse with historical purchase events, updated in batch daily
+        query=Query(
+            selects=select("merchant_id","account_age", "zipcode", "is_big_merchant", "country", "account_type", "preferred_language"), # Select the fields we care about
+        )
+    )
+)
+
+merchant_group_by = GroupBy(
+    sources=[source_merchants],
+    keys=["merchant_id"],
+    aggregations=None
+)
---
+++ api/py/test/sample/group_bys/risk/user_data.py
@@ -0,0 +1,29 @@
+from ai.chronon.api.ttypes import Source, EntitySource
+from ai.chronon.query import Query, select
+from ai.chronon.group_by import (
+    GroupBy,
+    Aggregation,
+    Operation,
+    Window,
+    TimeUnit
+)
+
+"""
+This GroupBy aggregates metrics about a user's previous purchases in various windows.
+"""
+
+# This source is raw purchase events. Every time a user makes a purchase, it will be one entry in this source.
+source_users = Source(
+    entities=EntitySource(
+        snapshotTable="data.users", # This points to the log table in the warehouse with historical purchase events, updated in batch daily
+        query=Query(
+            selects=select("user_id","account_age", "account_balance", "credit_score", "number_of_devices", "country", "account_type", "preferred_language"), # Select the fields we care about
+        ) # The event time
+    )
+)
+
+user_group_by = GroupBy(
+    sources=[source_users],
+    keys=["user_id"],
+    aggregations=None
+)
