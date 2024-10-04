824e8cd1392999a5bb248481b949157ea98cb8b0
chewy-zlai <chewys@zipline.ai>
2024-10-04 14:30:37 -0700

    Model api (#27)
    ## Summary
    
    ## Checklist
    - [x] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new `Model` function for constructing machine learning
    model objects with customizable parameters.
    - Added a `ModelType` class to encapsulate different model types such as
    XGBoost and PyTorch.
    - New model configuration `quickstart.test.v1` for defining output
    schemas and model parameters.
    
    - **Configurations**
    - New JSON configuration files for the `quickstart.test.v1` model,
    specifying output schema and source details.
    
    - **Tests**
    - Added tests for setting up training datasets and integrating with
    machine learning models.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: ezvz <vzanoyan@gmail.com>
    Co-authored-by: Chewy Shaw <chewys1024@gmail.com>

---
+++ api/py/test/sample/models/quickstart/test.py
@@ -0,0 +1,20 @@
+
+from ai.chronon.model import Model, ModelType
+from ai.chronon.api.ttypes import DataKind, EventSource, Source, TDataType
+from ai.chronon.query import Query, select
+
+
+"""
+This is the "left side" of the join that will comprise our training set. It is responsible for providing the primary keys
+and timestamps for which features will be computed.
+"""
+source = Source(
+    events=EventSource(
+        table="data.checkouts", 
+        query=Query(
+            selects=select("user_id"),
+            time_column="ts",
+            )
+    ))
+
+v1 = Model(source=source, outputSchema=TDataType(DataKind.DOUBLE), modelType=ModelType.XGBoost)
