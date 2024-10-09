3988837bad94879ad4677ac50609478dc1349260
Piyush Narang <piyush@zipline.ai>
2024-10-09 17:45:12 -0400

    Build.sbt tweaks to include cloud_aws module

--- hub/app/module/DynamoDBModule.scala
+++ hub/app/module/DynamoDBModule.scala
@@ -1,3 +1,12 @@
-package module class DynamoDBModule {
+package module
 
+import com.google.inject.AbstractModule
+import ai.chronon.integrations.aws.AwsApiImpl
+import play.api.{Configuration, Environment}
+
+class DynamoDBModule(environment: Environment, configuration: Configuration) extends AbstractModule {
+
+  override def configure(): Unit = {
+    bind(classOf[AwsApiImpl]).asEagerSingleton()
+  }
 }
