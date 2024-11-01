fadbae7d99231150b9bffe4150d7ca34a44dbdba
chewy-zlai <chewys@zipline.ai>
2024-11-01 14:37:23 -0700

    Table creation (#47)
    ## Summary
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [x] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced functionality to create a "drift_statistics" table in
    DynamoDB via command-line interface.
    - Added checks for required JAR files in the startup script to ensure
    proper initialization.
    
    - **Bug Fixes**
    - Updated user permissions for the DynamoDB service in the Docker
    configuration to enhance access control.
    
    - **Documentation**
    - Improved clarity in the startup script with new variable definitions
    for JAR file paths.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Chewy Shaw <chewys1024@gmail.com>

--- docker-init/start.sh
+++ docker-init/start.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+
+# Define variables for clarity
+SPARK_JAR="/app/cli/spark.jar"
+CLOUD_AWS_JAR="/app/cli/cloud_aws.jar"
+ONLINE_CLASS="ai.chronon.integrations.aws.AwsApiImpl"
+
+
 if ! python3.8 generate_anomalous_data.py; then
     echo "Error: Failed to generate anomalous data" >&2
     exit 1
 fi
+
+
+# Check if required JAR files exist
+if [[ ! -f $SPARK_JAR ]] || [[ ! -f $CLOUD_AWS_JAR ]]; then
+    echo "Error: Required JAR files not found" >&2
+    exit 1
+fi
+
 
 # Load up metadata into DynamoDB
 echo "Loading metadata.."
@@ -11,6 +26,17 @@ if ! java -cp /app/cli/spark.jar:$CLASSPATH ai.chronon.spark.Driver metadata-upl
   exit 1
 fi
 echo "Metadata load completed successfully!"
+
+# Initialize DynamoDB
+echo "Initializing DynamoDB Table .."
+if ! output=$(java -cp $SPARK_JAR:$CLASSPATH ai.chronon.spark.Driver create-stats-table \
+  --online-jar=$CLOUD_AWS_JAR \
+  --online-class=$ONLINE_CLASS 2>&1); then
+  echo "Error: Failed to bring up DynamoDB table" >&2
+  echo "Java command output: $output" >&2
+  exit 1
+fi
+echo "DynamoDB Table created successfully!"
 
 # Add these java options as without them we hit the below error:
 # throws java.lang.ClassFormatError accessible: module java.base does not "opens java.lang" to unnamed module @36328710
