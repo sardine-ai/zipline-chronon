4575984e21973da8ee5c4d759a1e164366e0079d
Ken Morton <ken@zipline.ai>
2024-12-13 09:22:47 -0500

    fix: use white as the navigation slider color (#127)
    ## Summary
    Changed the color of the top loading slider animation to white
    
    [Demo](https://drive.google.com/file/d/1zng_nK93YzT3fySVQfWpyb5VUorE3k8-/view?usp=sharing)
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Updated the background color of the navigation slider for improved
    visibility during navigation.
    
    - **Style**
    - Enhanced the visual appearance of the navigation slider based on
    navigation state.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- frontend/src/lib/components/NavigationSlider/NavigationSlider.svelte
+++ frontend/src/lib/components/NavigationSlider/NavigationSlider.svelte
@@ -6,7 +6,7 @@
 
 {#if $navigating}
 	<div
-		class="fixed w-full top-0 right-0 left-0 h-[1px] z-50 bg-primary"
+		class="fixed w-full top-0 right-0 left-0 h-[1px] z-50 bg-white"
 		in:slide={{ duration: 2000, axis: 'x', easing: expoOut }}
 	></div>
 {/if}
