558ab7e61132338950cd3cc54d974f43df68fc68
tchow <thomas@zipline.ai>
2024-12-13 14:34:46 -0800

    feat: implement Dataproc spark submitter utility (#122)
    ## Summary
    
    - Addresses https://app.asana.com/0/1208949807589885/1208949935294089
    - Introducing a `SparkSubmitter` abstraction, implementations should
    support both standalone and lib calls.
    - Added a unit test that can be used to test functionality locally.
    However, will require manually adding jars to gcs as part of it. I will
    improve this DX in a followup iteration.
    - The cluster counterpart to this is described in
    https://docs.google.com/document/d/1bxNUeaVrWDRL07fyLWiBDwO6pLS9BUbPbMLSnbRDRCk/edit?tab=t.0#heading=h.917ddfitv46i
    , note the CLI command to create the cluster in the proper way.
    - Next step is to prove this out with the quickstart, but the job
    submission already works.
     
    <img width="1652" alt="Screenshot 2024-12-12 at 4 40 59 PM"
    src="https://github.com/user-attachments/assets/3e178e1a-cccb-477e-800a-124f5b52e3b1"
    />
    
    
    ## Checklist
    - [x] Added Unit Tests
    - [x] Covered by existing CI
    - [x] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new configuration file for submitting jobs to Google
    Cloud Dataproc.
    - Added functionality for managing Spark jobs, including submission,
    status checking, and job cancellation.
    	- Implemented a test suite for the new job submission functionality.
    - Added a new test class for future testing of BigQuery catalog
    functionalities.
    
    - **Bug Fixes**
    	- Improved clarity in test assertions for better readability.
    	- Enhanced error handling in the job submission process.
    
    - **Documentation**
    - Enhanced configuration parameters for Google Cloud Dataproc job
    submissions.
    
    - **Chores**
    - Updated project dependencies and configurations for better integration
    and compatibility.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":"main"}
    ```
    -->

---
+++ cloud_gcp/src/test/scala/ai/chronon/integrations/cloud_gcp/test/BigQueryCatalogTest.scala
@@ -0,0 +1,6 @@
+package ai.chronon.integrations.cloud_gcp.test
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.mockito.MockitoSugar
+
+class BigQueryCatalogTest extends AnyFunSuite with MockitoSugar {}
