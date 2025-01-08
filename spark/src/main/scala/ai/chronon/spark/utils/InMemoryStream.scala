4608d34ee71e6aacbfc8156214a50b8e8c9f8e6e
varant-zlai <varant@zipline.ai>
2025-01-07 17:13:54 -0800

    [Untested] Vz  cherry pick oss avro schema (#170)
    ## Summary
    
    Cherry picking avro schema parsing improvements
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    - [x] Untested
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new `SchemaTraverser` trait to enhance schema traversal
    capabilities.
    	- Added advanced schema handling for Avro and Spark data conversions.
    
    - **Improvements**
    	- Enhanced row conversion methods with more flexible schema processing.
    	- Improved support for complex data types and schema representations.
    	- Updated encoding and conversion methods across multiple components.
    
    - **Technical Enhancements**
    - Implemented `AvroSchemaTraverser` for more robust Avro schema
    navigation.
    - Refined data conversion methods to support more flexible schema
    handling.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Spencer McNamara <smcnamara@stripe.com>
    Co-authored-by: ezvz <vzanoyan@gmail.com>

--- spark/src/main/scala/ai/chronon/spark/utils/InMemoryStream.scala
+++ spark/src/main/scala/ai/chronon/spark/utils/InMemoryStream.scala
@@ -100,7 +100,9 @@ class InMemoryStream {
     input.addData(inputDf.collect.map { row: Row =>
       val bytes =
         encodeRecord(avroSchema)(
-          AvroConversions.fromChrononRow(row, schema, GenericRowHandler.func).asInstanceOf[GenericData.Record])
+          AvroConversions
+            .fromChrononRow(row, schema, avroSchema, GenericRowHandler.func)
+            .asInstanceOf[GenericData.Record])
       bytes
     })
     input.toDF
