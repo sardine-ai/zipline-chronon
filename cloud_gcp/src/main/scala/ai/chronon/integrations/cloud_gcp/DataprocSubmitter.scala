095c9f6a8d95f97f14f5498cb0c35788a886bf15
Piyush Narang <piyush@zipline.ai>
2025-01-17 09:23:02 -0500

    First cut at some prod friendly Flink settings for our Flink apps (#226)
    ## Summary
    Added some settings that get things a bit more prod friendly - Covers
    items like checkpointing configs, TM memory sizes & breakdowns, max
    parallelism etc. We used some of these (e.g max parallelism,
    checkpointing frequency) at Stripe and they were useful in preventing
    OOMs and also ensuring low freshness latency (steady state as well as
    when triggered by restarts). I also picked some recommendations from -
    [Flink Production Readiness
    Checklist](https://nightlies.apache.org/flink/flink-docs-master/docs/ops/production_ready/).
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [X] Integration tested
    
    I was able to test these by kicking off a Flink job on our cluster and
    confirming the settings are being picked up and checkpoints are being
    triggered and written out to GCS as expected. Here's the [Flink app
    page](https://navizgjemzhanao4zth4oiybhy-dot-us-central1.dataproc.googleusercontent.com/gateway/default/yarn/proxy/application_1736797235235_0059/)
    if folks want to play around.
    
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Enhanced Flink job configuration with improved checkpointing and
    parallelism settings
      - Added metrics tracking for feature freshness in data processing
    
    - **Improvements**
    - Refined event sourcing mechanism for more controlled data stream
    generation
      - Updated dependency management for Flink streaming library
    
    - **Performance**
    - Optimized memory management and state backend configuration for Flink
    jobs
      - Introduced more granular control over task manager memory allocation
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- cloud_gcp/src/main/scala/ai/chronon/integrations/cloud_gcp/DataprocSubmitter.scala
+++ cloud_gcp/src/main/scala/ai/chronon/integrations/cloud_gcp/DataprocSubmitter.scala
@@ -100,8 +100,35 @@ class DataprocSubmitter(jobControllerClient: JobControllerClient, conf: Submitte
   }
 
   private def buildFlinkJob(mainClass: String, mainJarUri: String, jarUri: String, args: String*): Job.Builder = {
+
+    // TODO leverage a setting in teams.json when that's wired up
+    val checkpointsDir = "gs://zl-warehouse/flink-state"
+
+    // JobManager is primarily responsible for coordinating the job (task slots, checkpoint triggering) and not much else
+    // so 4G should suffice.
+    // We go with 64G TM containers (4 task slots per container)
+    // Broadly Flink splits TM memory into:
+    // 1) Metaspace, framework offheap etc
+    // 2) Network buffers
+    // 3) Managed Memory (rocksdb)
+    // 4) JVM heap
+    // We tune down the network buffers to 1G-2G (default would be ~6.3G) and use some of the extra memory for
+    // managed mem + jvm heap
+    // Good doc - https://nightlies.apache.org/flink/flink-docs-master/docs/deployment/memory/mem_setup_tm
     val envProps =
-      Map("jobmanager.memory.process.size" -> "4G", "yarn.classpath.include-user-jar" -> "FIRST")
+      Map(
+        "jobmanager.memory.process.size" -> "4G",
+        "taskmanager.memory.process.size" -> "64G",
+        "taskmanager.memory.network.min" -> "1G",
+        "taskmanager.memory.network.max" -> "2G",
+        "taskmanager.memory.managed.fraction" -> "0.5f",
+        "yarn.classpath.include-user-jar" -> "FIRST",
+        "state.savepoints.dir" -> checkpointsDir,
+        "state.checkpoints.dir" -> checkpointsDir,
+        // override the local dir for rocksdb as the default ends up being too large file name size wise
+        "state.backend.rocksdb.localdir" -> "/tmp/flink-state",
+        "state.checkpoint-storage" -> "filesystem"
+      )
 
     val flinkJob = FlinkJob
       .newBuilder()
