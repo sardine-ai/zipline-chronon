2010a0b4024021583d645f250c9c6594daba05c6
Nikhil Simha <nikhil@zipline.ai>
2025-02-13 00:41:31 -0800

    fix: write to gcs parquet instead of bq native (#371)
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced the table creation process to return clear, detailed
    statuses, improving feedback during table building.
    - Introduced a new method for generating table builders that integrates
    with BigQuery, including error handling for partitioning.
    - Streamlined data writing operations to cloud storage with automatic
    path configuration and Parquet integration.
    - Added explicit partitioning for DataFrame saves in Hive, Delta, and
    Iceberg formats.
      
    - **Refactor**
    - Overhauled logic to enforce partition restrictions and incorporate
    robust error handling for a smoother user experience.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: tchow-zlai <thomas@zipline.ai>
    Co-authored-by: Thomas Chow <thomaschow369@gmail.com>

--- cloud_gcp/src/main/scala/ai/chronon/integrations/cloud_gcp/GCSFormat.scala
+++ cloud_gcp/src/main/scala/ai/chronon/integrations/cloud_gcp/GCSFormat.scala
@@ -1,14 +1,32 @@
 package ai.chronon.integrations.cloud_gcp
 
+import ai.chronon.api.Extensions.StringOps
+import ai.chronon.api.ScalaJavaConversions.JListOps
+import ai.chronon.spark.TableUtils
+import ai.chronon.spark.TableUtils.{TableCreatedWithInitialData, TableCreationStatus}
 import ai.chronon.spark.format.Format
-import org.apache.spark.sql.Encoders
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.SparkSession
+import com.google.cloud.bigquery.connector.common.BigQueryUtil
+import com.google.cloud.spark.bigquery.repackaged.com.google.cloud.bigquery.{
+  BigQuery,
+  BigQueryOptions,
+  ExternalTableDefinition,
+  FormatOptions,
+  HivePartitioningOptions,
+  TableInfo
+}
+import com.google.cloud.spark.bigquery.{SchemaConverters, SchemaConvertersConfiguration}
+import org.apache.spark.sql.{DataFrame, Encoders, Row, SparkSession}
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 import org.apache.spark.sql.execution.FileSourceScanExec
 import org.apache.spark.sql.execution.datasources.PartitioningAwareFileIndex
+import org.slf4j.LoggerFactory
 
 case class GCS(sourceUri: String, fileFormat: String) extends Format {
+
+  private lazy val logger = LoggerFactory.getLogger(this.getClass.getName)
+
+  private lazy val bqOptions = BigQueryOptions.getDefaultInstance
+  lazy val bigQueryClient: BigQuery = bqOptions.getService
 
   override def name: String = fileFormat
 
@@ -70,6 +88,62 @@ case class GCS(sourceUri: String, fileFormat: String) extends Format {
           fieldName -> fieldValue.toString // Just going to cast this as a string.
 
         }.toMap)
+  }
+
+  override def generateTableBuilder(df: DataFrame,
+                                    tableName: String,
+                                    partitionColumns: Seq[String],
+                                    tableProperties: Map[String, String],
+                                    fileFormat: String): (String => Unit) => TableCreationStatus = {
+
+    def inner(df: DataFrame, tableName: String, partitionColumns: Seq[String])(sqlEvaluator: String => Unit) = {
+
+      // See: https://cloud.google.com/bigquery/docs/partitioned-tables#limitations
+      // "BigQuery does not support partitioning by multiple columns. Only one column can be used to partition a table."
+      require(partitionColumns.size < 2,
+              s"BigQuery only supports at most one partition column, incoming spec: ${partitionColumns}")
+
+      val shadedTableId = BigQueryUtil.parseTableId(tableName)
+
+      val writePrefix = TableUtils(df.sparkSession).writePrefix
+      require(writePrefix.nonEmpty, "Please set conf 'spark.chronon.table_write.prefix' pointing to a data bucket.")
+
+      val path = writePrefix.get + tableName.sanitize + "/" //split("/").map(_.sanitize).mkString("/")
+      val dataGlob = path + "*"
+
+      logger.info(s"""
+           |table source uri: $dataGlob
+           |partition uri: $path
+           |""".stripMargin)
+
+      df.write
+        .partitionBy(partitionColumns: _*)
+        .mode("overwrite") // or "append" based on your needs
+        .parquet(path)
+
+      val baseTableDef = ExternalTableDefinition
+        .newBuilder(dataGlob, FormatOptions.parquet())
+        .setAutodetect(true)
+
+      if (partitionColumns.nonEmpty) {
+        val timePartitioning = HivePartitioningOptions
+          .newBuilder()
+          .setFields(partitionColumns.toJava)
+          .setSourceUriPrefix(path)
+          .setMode("STRINGS")
+          .build()
+        baseTableDef.setHivePartitioningOptions(timePartitioning)
+      }
+
+      val tableInfo = TableInfo.newBuilder(shadedTableId, baseTableDef.build).build()
+      val createdTable = bigQueryClient.create(tableInfo)
+
+      println(s"Created external table ${createdTable.getTableId}")
+
+      TableCreatedWithInitialData
+    }
+
+    inner(df, tableName, partitionColumns)
   }
 
   def createTableTypeString: String = throw new UnsupportedOperationException("GCS does not support create table")
