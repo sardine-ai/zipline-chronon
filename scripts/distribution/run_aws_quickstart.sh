fb40121315d939f2db6a5a6a2bbf728690cc3074
David Han <david@zipline.ai>
2025-03-06 11:34:02 -0500

    Add integration testing

---
+++ scripts/distribution/run_aws_quickstart.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+# Run this bash script via the python file `distribution/run_zipline_quickstart.py`
+set -xo pipefail
+
+WORKING_DIR=$1
+cd $WORKING_DIR
+
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+
+WHEEL_FILE="zipline_ai-0.1.0-py3-none-any.whl "
+
+# Delete glue tables to start from scratch
+#bq rm -f -t canary-443022:data.quickstart_purchases_v1_test
+#TODO: delete bigtable rows
+
+# Clone the cananry configs
+git clone git@github.com:zipline-ai/cananry-confs.git
+cd cananry-confs
+
+# Use the branch with Zipline specific team.json
+git fetch origin davidhan/run_aws
+git checkout davidhan/run_aws
+
+# Create a virtualenv to fresh install zipline-ai
+python3 -m venv tmp_chronon
+source tmp_chronon/bin/activate
+
+# Download the wheel
+aws s3 cp s3://zipline-artifacts-canary/jars/$WHEEL_FILE .
+
+# Install the wheel (force)
+pip uninstall zipline-ai
+pip install --force-reinstall $WHEEL_FILE
+
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
+# function to check dataproc job id state
+function check_dataproc_job_state() {
+  JOB_ID=$1
+  if [ -z "$JOB_ID" ]; then
+        echo "No job id available to check. Exiting."
+        exit 1
+  fi
+  echo -e "${GREEN} <<<<<<<<<<<<<<<<-----------------JOB STATUS----------------->>>>>>>>>>>>>>>>>\033[0m"
+  JOB_STATE=$(gcloud dataproc jobs describe $JOB_ID --region=us-central1 --format=flattened | grep "status.state:")
+  echo $JOB_STATE
+#  TODO: this doesn't actually fail. need to fix.
+  if [ -z "$JOB_STATE" ]; then
+        echo "Job failed"
+        exit 1
+  fi
+}
+
+function check_emr_step_state() {
+  STEP_ID=$1
+  CLUSTER_ID=$2
+  if [ -z "$STEP_ID" ]; then
+        echo "No step id available to check. Exiting."
+        exit 1
+  fi
+  if [ -z "$CLUSTER_ID" ]; then
+        echo "No cluster id available to check. Exiting."
+        exit 1
+  fi
+  echo -e "${GREEN} <<<<<<<<<<<<<<<<-----------------JOB STATUS----------------->>>>>>>>>>>>>>>>>\033[0m"
+  # check exit code
+  if ! aws emr wait step-complete --cluster-id $CLUSTER_ID --step-id $STEP_ID; then
+        echo "Job failed"
+        exit 1
+  fi
+  aws emr describe-step --cluster-id $CLUSTER_ID --step-id $STEP_ID
+}
+
+function check_emr_cluster_state() {
+  CLUSTER_ID=$1
+  if [ -z "$CLUSTER_ID" ]; then
+        echo "No cluster id available to check. Exiting."
+        exit 1
+  fi
+  echo -e "${GREEN} <<<<<<<<<<<<<<<<-----------------JOB STATUS----------------->>>>>>>>>>>>>>>>>\033[0m"
+  if ! aws emr wait cluster-running --cluster-id $CLUSTER_ID; then
+        echo "Cluster failed"
+        exit 1
+  fi
+  aws emr describe-cluster --cluster-id $CLUSTER_ID
+}
+
+EMR_SUBMITTER_ID_CLUSTER_STR="EMR job id"
+EMR_SUBMITER_ID_STEP_STR="EMR step id"
+
+echo -e "${GREEN}<<<<<.....................................COMPILE.....................................>>>>>\033[0m"
+zipline compile --conf=group_bys/quickstart/purchases.py
+
+echo -e "${GREEN}<<<<<.....................................BACKFILL.....................................>>>>>\033[0m"
+touch tmp_backfill.out
+zipline run --conf production/group_bys/quickstart/purchases.v1_dev --create-cluster --cluster-instance-count=1 --cluster-idle-timeout=1 2>&1 | tee tmp_backfill.out
+CLUSTER_ID=$(cat tmp_backfill.out | grep "$EMR_SUBMITTER_ID_CLUSTER_STR"  | cut -d " " -f5)
+check_emr_cluster_state $CLUSTER_ID
+
+# Get the step id
+STEP_ID=$(aws emr list-steps --cluster-id $CLUSTER_ID | jq -r '.Steps[0].Id')
+check_emr_step_state $STEP_ID $CLUSTER_ID
+
+
+
--- scripts/distribution/run_zipline_quickstart.py
+++ scripts/distribution/run_zipline_quickstart.py
@@ -1,19 +1,39 @@
+import argparse
 import tempfile
 import subprocess
 import os
 
+from enum import Enum
 
-def main():
+GCP = 'GCP'
+AWS = 'AWS'
+
+def main(cloud):
     with tempfile.TemporaryDirectory() as temp_dir:
         print(f"Created temporary directory: {temp_dir}")
 
-        quickstart_sh = os.path.join(os.path.dirname(os.path.realpath(__file__))
-                                     , "run_zipline_quickstart.sh")
+        if cloud == GCP:
+            # set the path to the run_gcp_quickstart.sh script
+            quickstart_sh = os.path.join(os.path.dirname(os.path.realpath(__file__))
+                                         , "run_gcp_quickstart.sh")
+        elif cloud == AWS:
+            # set the path to the run_aws_quickstart.sh script
+            quickstart_sh = os.path.join(os.path.dirname(os.path.realpath(__file__))
+                                         , "run_aws_quickstart.sh")
 
-        # run the bash script run_zipline_quickstart.sh subprocess command
+        # run the bash script run_gcp_quickstart.sh subprocess command
         # with the temporary directory as the argument
         subprocess.run([f"bash {quickstart_sh} {temp_dir}"], shell=True)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        prog='Run zipline quickstart')
+    parser.add_argument(
+        "--cloud",
+        choices=[GCP, AWS],
+        help="Cloud provider to run tests on",
+        required=True
+    )
+    args = parser.parse_args()
+    main(args.cloud.upper())
