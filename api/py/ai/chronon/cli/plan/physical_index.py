9b3e11a87e061429ce347b1c8bea0e5e7e3269fc
Nikhil Simha <nikhil@zipline.ai>
2025-03-18 18:39:57 -0700

    stub/part_1: physical graph for workflow submission + column lineage (#493)
    ## Summary
    
    Adding function stubs and thrift schemas for orchestration with column level lineage.
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Streamlined CLI operations with a simplified backfill command and
    removal of an obsolete synchronization command.
    - Enhanced configuration processing that now automatically compiles and
    returns results.
    - Expanded physical planning capabilities with new mechanisms for
    managing computation graphs.
    - Extended tracking of data transformations through improved column
    lineage support in metadata.
    - Introduction of new classes and methods for managing physical nodes
    and graphs, enhancing data processing workflows.
    - New interface for orchestrators to handle configuration fetching and
    uploading.
    - New structures for detailed column lineage tracking throughout data
    processing stages.
    - Added support for generating Python code from new Thrift definitions
    in the testing workflow.
    
    - **Bug Fixes**
    - Improved documentation for clarity on the functionality and logic of
    certain fields.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: ezvz <vzanoyan@gmail.com>

---
+++ api/py/ai/chronon/cli/plan/physical_index.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+from ai.chronon.api.common.ttypes import ConfigType
+from ai.chronon.cli.compile.compiler import CompileResult
+from ai.chronon.cli.plan.controller_iface import ControllerIface
+from ai.chronon.cli.plan.physical_graph import PhysicalGraph
+from ai.chronon.lineage.ttypes import Column, ColumnLineage
+from ai.chronon.cli.plan.physical_node import PhysicalNode
+
+
+@dataclass
+class PhysicalIndex:
+    table_to_physical: Dict[str, PhysicalNode]
+
+    # TODO incorporate stage column lineage
+    column_lineage: Dict[Column, ColumnLineage]
+    controller: ControllerIface
+
+    def __init__(
+        self,
+        physical_nodes: List[PhysicalNode],
+        controller: ControllerIface,
+        branch: str,
+    ):
+        self.controller = controller
+        self.table_to_physical = {}
+        self.column_lineage = {}
+        self.branch = branch
+        self.populate_index(physical_nodes)
+
+    # TODO: populate index
+    def populate_index(self, physical_nodes: List[PhysicalNode]):
+        raise NotImplementedError("Method not yet implemented")
+
+    @classmethod
+    def from_compiled_obj(
+        cls, compiled_obj: Dict[ConfigType, CompileResult]
+    ) -> "PhysicalIndex":
+        raise NotImplementedError("Method not yet implemented")
+
+    def get_backfill_physical_graph(
+        self, conf_name: str, start_date: str, end_date: str
+    ) -> PhysicalGraph:
+        raise NotImplementedError("Method not yet implemented")
+
+    def get_deploy_physical_graph(self, conf_name: str, date: str) -> PhysicalGraph:
+        raise NotImplementedError("Method not yet implemented")
+    def submit_physical_graph(self, physical_graph: PhysicalGraph) -> str:
+
+        node_to_physical: Dict[str, PhysicalNode] = physical_graph.flatten()
+
+        node_to_hash = {name: node.conf_hash for name, node in node_to_physical.items()}
+
+        missing_conf_names = self.controller.fetch_missing_confs(node_to_hash)
+        missing_physical_nodes = [
+            node_to_physical[conf_name] for conf_name in missing_conf_names
+        ]
+
+        # upload missing confs
+        for physical_node in missing_physical_nodes:
+            hash = physical_node.conf_hash
+            json = physical_node.conf.tjson
+            name = physical_node.name
+            self.controller.upload_conf(name, hash, json)
