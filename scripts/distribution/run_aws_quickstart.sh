a6033b3327844dceccab1a01d7e90267dc94a472
chewy-zlai <chewys@zipline.ai>
2025-03-21 11:35:46 -0700

    Canary Automated Testing  (#471)
    ## Summary
    
    This updates the Push To Canary workflow to push the updated jars and
    wheel to the cloud canaries and run integration tests.
    
    The jobs are as follows:
    * **build_artifacts** 
    builds the jars and wheel from the main branch and uploads them to
    github for use in separate jobs simultaneously
    * **push_to_aws**
    downloads the jars and wheel from github and uploads them to the s3
    zipline-artifacts-canary bucket under version "candidate"
    * **push_to_gcp**
    downloads the jars and wheel from github and uploads them to the gcs
    zipline-artifacts-canary bucket under version "candidate"
    * **run_aws_integration_tests**
    runs the quickstart compile and backfill based on
    [scripts/distribution/run_aws_quickstart.sh](https://github.com/zipline-ai/chronon/blob/main/scripts/distribution/run_aws_quickstart.sh)
    * **run_gcp_integration_tests**
    runs the quickstart compile, backfill, group-by-upload, upload-to-kv,
    metadata-upload, and fetch based on
    [scripts/distribution/run_gcp_quickstart.sh](https://github.com/zipline-ai/chronon/blob/main/scripts/distribution/run_gcp_quickstart.sh)
    * **clean_up_artifacts**
    removes the jars and wheel from github to minimize the storage costs.
    * **merge_to_main-passing-tests**
    if the tests all pass, this will merge the latest commits to the branch
    "main-passing-tests". This also creates a txt file artifact marking
    which commit sha passed which is saved by github.
    * **on_fail_notify_slack**
    if the tests fail, this will send a notification to the
    "integration-tests" channel of our Slack.
    
    A test run of the workflow can be seen here:
    https://github.com/zipline-ai/chronon/actions/runs/13913346353
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [x] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **Chores**
    - Enhanced our release pipeline to streamline package generation and
    distribution.
    	- Automated deployment of new build packages across AWS and GCP.
    	- Introduced cleanup routines to optimize the post-deployment process.
    - Updated job structure for improved artifact management and deployment.
    - Added new jobs for building artifacts and uploading them to cloud
    storage, including `build_artifacts`, `push_to_aws`, `push_to_gcp`, and
    `clean_up_artifacts`.
    	- Defined outputs for generated artifacts to improve tracking.
    - Improved flexibility in deployment scripts for different environments
    (dev and canary).
    - Updated scripts to conditionally handle resource management based on
    environment settings.
    - Introduced environment-specific configurations for AWS and GCP
    operations.
    	- Added integration testing jobs for AWS and GCP post-deployment.
    - Enhanced scripts for AWS and GCP to support environment-specific
    operations and configurations.
    - Introduced a mechanism to switch between different environments
    affecting resource management and command configurations.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- scripts/distribution/run_aws_quickstart.sh
+++ scripts/distribution/run_aws_quickstart.sh
@@ -7,22 +7,31 @@ create_cluster=false
 # otherwise use the canary cluster id
 CANARY_CLUSTER_ID="j-13BASWFP15TLR"
 
+ENVIRONMENT="dev"
+
 # Loop through all arguments
 for arg in "$@"; do
   if [ "$arg" = "--create-cluster" ]; then
     create_cluster=true
-    break
+  fi
+  if [[ "$arg" == "--canary" ]]; then
+    ENVIRONMENT="canary"
   fi
 done
 
 # Delete glue tables
 # hardcoding the s3 path here because that's where the underlying location of the data is for this glue database `data`
 # Faster to just aws s3 rm than to aws glue delete-table-version + delete-partition and then finally delete-table
-aws s3 rm s3://zipline-warehouse-canary/data/aws_plaid_fv_v1 --recursive
-aws glue delete-table --database-name data --name aws_plaid_fv_v1
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  aws s3 rm s3://zipline-warehouse-canary/data/aws_purchases_v1_test --recursive
+  aws glue delete-table --database-name data --name quickstart_purchases_v1_test
+else
+  aws s3 rm s3://zipline-warehouse-dev/data/aws_purchases_v1_dev --recursive
+  aws glue delete-table --database-name data --name quickstart_purchases_v1_dev
+fi
 
-#aws s3 rm s3://zipline-warehouse-canary/data/aws_purchases_v1_dev --recursive
-#aws glue delete-table --database-name data --name aws_purchases_v1_dev
+#aws s3 rm s3://zipline-warehouse-canary/data/aws_plaid_fv_v1 --recursive
+#aws glue delete-table --database-name data --name aws_plaid_fv_v1
 
 
 # Sleep for a bit since aws glue delete-table is asynchronous
@@ -31,7 +40,6 @@ sleep 30
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 
-WHEEL_FILE="zipline_ai-0.1.0-py3-none-any.whl"
 
 #git fetch origin davidhan/run_aws
 #git checkout davidhan/run_aws
@@ -44,7 +52,13 @@ python3 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 
 # Download the wheel
-aws s3 cp s3://zipline-artifacts-dev/release/latest/wheels/$WHEEL_FILE .
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  WHEEL_FILE=$(aws s3 ls s3://zipline-artifacts-canary/release/candidate/wheels/ | grep zipline_ai | sort | tail -n 1 | awk '{print $4}')
+  aws s3 cp s3://zipline-artifacts-canary/release/candidate/wheels/$WHEEL_FILE .
+else
+  WHEEL_FILE=$(aws s3 ls s3://zipline-artifacts-dev/release/latest/wheels/ | grep zipline_ai | sort | tail -n 1 | awk '{print $4}')
+  aws s3 cp s3://zipline-artifacts-dev/release/latest/wheels/$WHEEL_FILE .
+fi
 
 # Install the wheel (force)
 pip uninstall zipline-ai
@@ -114,15 +128,22 @@ CHRONON_ROOT=`pwd`/api/py/test/canary
 export PYTHONPATH="${PYTHONPATH}:$CHRONON_ROOT"
 
 echo -e "${GREEN}<<<<<.....................................COMPILE.....................................>>>>>\033[0m"
-zipline compile --repo=$CHRONON_ROOT --conf=group_bys/aws/plaid_fv.py
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline compile --repo=$CHRONON_ROOT --conf=group_bys/aws/purchases.py
+else
+  zipline compile --repo=$CHRONON_ROOT --conf=group_bys/aws/purchases.py
+fi
 
 echo -e "${GREEN}<<<<<.....................................BACKFILL.....................................>>>>>\033[0m"
 touch tmp_backfill.out
 
 if [ "$create_cluster" = true ]; then
   echo "Creating a new EMR cluster"
-#  zipline run --conf production/group_bys/aws/purchases.v1_dev --create-cluster --cluster-instance-count=2 --cluster-idle-timeout=60 2>&1 | tee tmp_backfill.out
-  zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/plaid_fv.v1 --end-ds 20250220 --create-cluster --cluster-instance-count=2 --cluster-idle-timeout=60 2>&1 | tee tmp_backfill.out
+  if [[ "$ENVIRONMENT" == "canary" ]]; then
+    zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/purchases.v1_test --end-ds 20250220 --create-cluster --cluster-instance-count=2 --cluster-idle-timeout=60 2>&1 | tee tmp_backfill.out
+  else
+    zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/purchases.v1_dev --end-ds 20250220 --create-cluster --cluster-instance-count=2 --cluster-idle-timeout=60 2>&1 | tee tmp_backfill.out
+  fi
   EMR_SUBMITTER_ID_CLUSTER_STR="EMR job id"
   CLUSTER_ID=$(cat tmp_backfill.out | grep "$EMR_SUBMITTER_ID_CLUSTER_STR"  | cut -d " " -f4) # expecting the cluster id to be the 4th field
   check_emr_cluster_state $CLUSTER_ID
@@ -132,8 +153,12 @@ if [ "$create_cluster" = true ]; then
 else
   CLUSTER_ID=$CANARY_CLUSTER_ID
   echo "Using existing EMR cluster $CLUSTER_ID"
-#  EMR_CLUSTER_ID=$CLUSTER_ID zipline run --conf production/group_bys/aws/purchases.v1_dev 2>&1 | tee tmp_backfill.out
-  EMR_CLUSTER_ID=$CLUSTER_ID zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/plaid_fv.v1 --end-ds 20250220 2>&1 | tee tmp_backfill.out
+  if [[ "$ENVIRONMENT" == "canary" ]]; then
+    EMR_CLUSTER_ID=$CLUSTER_ID zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/purchases.v1_test --end-ds 20250220 2>&1 | tee tmp_backfill.out
+  else
+    EMR_CLUSTER_ID=$CLUSTER_ID zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/aws/purchases.v1_dev --end-ds 20250220 2>&1 | tee tmp_backfill.out
+  fi
+
   EMR_SUBMITER_ID_STEP_STR="EMR step id"
   STEP_ID=$(cat tmp_backfill.out | grep "$EMR_SUBMITER_ID_STEP_STR"  | cut -d " " -f4) # expecting the step id to be the 4th field
   check_emr_step_state $STEP_ID $CLUSTER_ID
--- scripts/distribution/run_gcp_quickstart.sh
+++ scripts/distribution/run_gcp_quickstart.sh
@@ -5,11 +5,23 @@ set -xo pipefail
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 
-WHEEL_FILE="zipline_ai-0.1.0-py3-none-any.whl "
+ENVIRONMENT="dev"
+
+# Loop through all arguments
+for arg in "$@"; do
+  if [[ "$arg" == "--canary" ]]; then
+    ENVIRONMENT="canary"
+  fi
+done
 
 # Delete gcp tables to start from scratch
-bq rm -f -t canary-443022:data.gcp_purchases_v1_dev
-bq rm -f -t canary-443022:data.gcp_purchases_v1_dev_upload
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  bq rm -f -t canary-443022:data.gcp_purchases_v1_test
+  bq rm -f -t canary-443022:data.gcp_purchases_v1_test_upload
+else
+  bq rm -f -t canary-443022:data.gcp_purchases_v1_dev
+  bq rm -f -t canary-443022:data.gcp_purchases_v1_dev_upload
+fi
 #TODO: delete bigtable rows
 
 # Create a virtualenv to fresh install zipline-ai
@@ -19,8 +31,13 @@ python3 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 
 # Download the wheel
-gcloud storage cp gs://zipline-artifacts-dev/jars/$WHEEL_FILE .
-
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  WHEEL_FILE=$(gcloud storage ls gs://zipline-artifacts-canary/release/candidate/wheels/ | grep zipline_ai | sort | tail -n 1 | xargs -n 1 basename)
+  gcloud storage cp gs://zipline-artifacts-canary/release/candidate/wheels/${WHEEL_FILE} .
+else
+  WHEEL_FILE=$(gcloud storage ls gs://zipline-artifacts-dev/release/latest/wheels/ | grep zipline_ai | sort | tail -n 1 | xargs -n 1 basename)
+  gcloud storage cp gs://zipline-artifacts-dev/release/latest/wheels/${WHEEL_FILE} .
+fi
 # Install the wheel (force)
 pip uninstall zipline-ai
 pip install --force-reinstall $WHEEL_FILE
@@ -54,33 +71,53 @@ zipline compile --chronon_root=$CHRONON_ROOT --conf=group_bys/gcp/purchases.py
 
 echo -e "${GREEN}<<<<<.....................................BACKFILL.....................................>>>>>\033[0m"
 touch tmp_backfill.out
-zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/gcp/purchases.v1_dev --dataproc 2>&1 | tee tmp_backfill.out
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/gcp/purchases.v1_test --dataproc 2>&1 | tee tmp_backfill.out
+else
+  zipline run --repo=$CHRONON_ROOT --mode backfill --conf production/group_bys/gcp/purchases.v1_dev --dataproc 2>&1 | tee tmp_backfill.out
+fi
 BACKFILL_JOB_ID=$(cat tmp_backfill.out | grep "$DATAPROC_SUBMITTER_ID_STR"  | cut -d " " -f5)
 check_dataproc_job_state $BACKFILL_JOB_ID
 
 echo -e "${GREEN}<<<<<.....................................GROUP-BY-UPLOAD.....................................>>>>>\033[0m"
 touch tmp_gbu.out
-zipline run --repo=$CHRONON_ROOT --mode upload --conf production/group_bys/gcp/purchases.v1_dev --ds  2023-12-01 --dataproc 2>&1 | tee tmp_gbu.out
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline run --repo=$CHRONON_ROOT --mode upload --conf production/group_bys/gcp/purchases.v1_test --ds  2023-12-01 --dataproc 2>&1 | tee tmp_gbu.out
+else
+  zipline run --repo=$CHRONON_ROOT --mode upload --conf production/group_bys/gcp/purchases.v1_dev --ds  2023-12-01 --dataproc 2>&1 | tee tmp_gbu.out
+fi
 GBU_JOB_ID=$(cat tmp_gbu.out | grep "$DATAPROC_SUBMITTER_ID_STR" | cut -d " " -f5)
 check_dataproc_job_state $GBU_JOB_ID
 
 # Need to wait for upload to finish
 echo -e "${GREEN}<<<<<.....................................UPLOAD-TO-KV.....................................>>>>>\033[0m"
 touch tmp_upload_to_kv.out
-zipline run --repo=$CHRONON_ROOT --mode upload-to-kv --conf production/group_bys/gcp/purchases.v1_dev --partition-string=2023-12-01 --dataproc 2>&1 | tee tmp_upload_to_kv.out
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline run --repo=$CHRONON_ROOT --mode upload-to-kv --conf production/group_bys/gcp/purchases.v1_test --partition-string=2023-12-01 --dataproc 2>&1 | tee tmp_upload_to_kv.out
+else
+  zipline run --repo=$CHRONON_ROOT --mode upload-to-kv --conf production/group_bys/gcp/purchases.v1_dev --partition-string=2023-12-01 --dataproc 2>&1 | tee tmp_upload_to_kv.out
+fi
 UPLOAD_TO_KV_JOB_ID=$(cat tmp_upload_to_kv.out | grep "$DATAPROC_SUBMITTER_ID_STR" | cut -d " " -f5)
 check_dataproc_job_state $UPLOAD_TO_KV_JOB_ID
 
 echo -e "${GREEN}<<<<< .....................................METADATA-UPLOAD.....................................>>>>>\033[0m"
 touch tmp_metadata_upload.out
-zipline run --repo=$CHRONON_ROOT --mode metadata-upload --conf production/group_bys/gcp/purchases.v1_dev --dataproc 2>&1 | tee tmp_metadata_upload.out
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline run --repo=$CHRONON_ROOT --mode metadata-upload --conf production/group_bys/gcp/purchases.v1_test --dataproc 2>&1 | tee tmp_metadata_upload.out
+else
+  zipline run --repo=$CHRONON_ROOT --mode metadata-upload --conf production/group_bys/gcp/purchases.v1_dev --dataproc 2>&1 | tee tmp_metadata_upload.out
+fi
 METADATA_UPLOAD_JOB_ID=$(cat tmp_metadata_upload.out | grep "$DATAPROC_SUBMITTER_ID_STR" | cut -d " " -f5)
 check_dataproc_job_state $METADATA_UPLOAD_JOB_ID
 
 # Need to wait for upload-to-kv to finish
 echo -e "${GREEN}<<<<<.....................................FETCH.....................................>>>>>\033[0m"
 touch tmp_fetch.out
-zipline run --repo=$CHRONON_ROOT --mode fetch --conf=production/group_bys/gcp/purchases.v1_dev -k '{"user_id":"5"}' --name gcp.purchases.v1_dev 2>&1 | tee tmp_fetch.out | grep -q purchase_price_average_14d
+if [[ "$ENVIRONMENT" == "canary" ]]; then
+  zipline run --repo=$CHRONON_ROOT --mode fetch --conf=production/group_bys/gcp/purchases.v1_test -k '{"user_id":"5"}' --name gcp.purchases.v1_test  2>&1 | tee tmp_fetch.out | grep -q purchase_price_average_14d
+else
+  zipline run --repo=$CHRONON_ROOT --mode fetch --conf=production/group_bys/gcp/purchases.v1_dev  -k '{"user_id":"5"}' --name gcp.purchases.v1_dev  2>&1 | tee tmp_fetch.out | grep -q purchase_price_average_14d
+fi
 cat tmp_fetch.out | grep purchase_price_average_14d
 # check if exit code of previous is 0
 if [ $? -ne 0 ]; then
