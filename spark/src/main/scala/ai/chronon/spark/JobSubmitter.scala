b9b918fad3bdebc97b181aa9654b7793fd6f300f
david-zlai <david@zipline.ai>
2025-04-07 12:07:32 -0400

    Use execution spark configs from compiled confs in JobSubmitter (#549)
    ## Summary
    
    ## Checklist
    - [ ] Added Unit Tests
    - [x] Covered by existing CI
    - [x] Integration tested
    - [ ] Documentation update
    
    Can see the spark properties set in the Configuration tab of this
    dataproc job:
    https://console.cloud.google.com/dataproc/jobs/3878fd24-6ca8-41df-8507-ecc7adcead91/configuration?region=us-central1&invt=Abt0gg&project=canary-443022
    
    - removed additional-confs.yaml integration
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
      - Enhanced configuration metadata with improved categorization.
    - Introduced dedicated submission properties for more robust cloud job
    configuration.
      - Added a new optional field `confType` to the metadata structure.
      - Introduced a new enumeration `ConfType` for configuration types.
    
    - **Refactor**
    - Streamlined job submission workflows and command argument handling
    across platforms.
    - Simplified Spark session setup by removing extraneous configuration
    file processing.
    
    - **Chores**
      - Updated logging dependencies for the cloud GCP target.
      - Modified artifact upload paths to streamline deployment processes.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Nikhil Simha <nikhil@zipline.ai>
    Co-authored-by: Thomas Chow <thomas@zipline.ai>

--- spark/src/main/scala/ai/chronon/spark/JobSubmitter.scala
+++ spark/src/main/scala/ai/chronon/spark/JobSubmitter.scala
@@ -1,16 +1,70 @@
 package ai.chronon.spark
 
+import ai.chronon.api
+import ai.chronon.api.ScalaJavaConversions.MapOps
+import ai.chronon.spark.JobSubmitterConstants.ConfTypeArgKeyword
+import ai.chronon.spark.JobSubmitterConstants.LocalConfPathArgKeyword
+import ai.chronon.spark.JobSubmitterConstants.OriginalModeArgKeyword
+
 sealed trait JobType
 case object SparkJob extends JobType
 case object FlinkJob extends JobType
 
 trait JobSubmitter {
 
-  def submit(jobType: JobType, jobProperties: Map[String, String], files: List[String], args: String*): String
+  def submit(jobType: JobType,
+             submissionProperties: Map[String, String],
+             jobProperties: Map[String, String],
+             files: List[String],
+             args: String*): String
 
   def status(jobId: String): Unit
 
   def kill(jobId: String): Unit
+}
+
+object JobSubmitter {
+  def getArgValue(args: Array[String], argKeyword: String): Option[String] = {
+    args
+      .find(_.startsWith(argKeyword))
+      .map(_.split("="))
+      .map(_(1))
+  }
+
+  def getModeConfigProperties(args: Array[String]): Option[Map[String, String]] = {
+    val localConfPathValue = getArgValue(args, LocalConfPathArgKeyword)
+    val confTypeValue = getArgValue(args, ConfTypeArgKeyword)
+
+    val modeConfigProperties = if (localConfPathValue.isDefined && confTypeValue.isDefined) {
+      val executionInfo = confTypeValue.get match {
+        case "joins"           => Driver.parseConf[api.Join](localConfPathValue.get).metaData.executionInfo
+        case "group_bys"       => Driver.parseConf[api.GroupBy](localConfPathValue.get).metaData.executionInfo
+        case "staging_queries" => Driver.parseConf[api.StagingQuery](localConfPathValue.get).metaData.executionInfo
+        case "models"          => Driver.parseConf[api.Model](localConfPathValue.get).metaData.executionInfo
+        case _                 => throw new Exception("Invalid conf type")
+      }
+
+      val originalMode = getArgValue(args, OriginalModeArgKeyword)
+
+      (Option(executionInfo.conf), originalMode) match {
+        case (Some(conf), Some(mode)) =>
+          Option(conf.getModeConfigs).map(modeConfigs => {
+            if (modeConfigs.containsKey(mode)) {
+              modeConfigs.get(mode).toScala
+            } else {
+              // check common
+              if (conf.isSetCommon) {
+                conf.getCommon.toScala
+              } else {
+                Map[String, String]()
+              }
+            }
+          })
+        case _ => None
+      }
+    } else None
+    modeConfigProperties
+  }
 }
 
 abstract class JobAuth {
@@ -38,4 +92,18 @@ object JobSubmitterConstants {
   val FlinkMainJarUriArgKeyword = "--flink-main-jar-uri"
   val FlinkSavepointUriArgKeyword = "--savepoint-uri"
   val FilesArgKeyword = "--files"
+  val ConfTypeArgKeyword = "--conf-type"
+  val LocalConfPathArgKeyword = "--local-conf-path"
+  val OriginalModeArgKeyword = "--original-mode"
+
+  val SharedInternalArgs: Set[String] = Set(
+    JarUriArgKeyword,
+    JobTypeArgKeyword,
+    MainClassKeyword,
+    FlinkMainJarUriArgKeyword,
+    FlinkSavepointUriArgKeyword,
+    LocalConfPathArgKeyword,
+    OriginalModeArgKeyword,
+    FilesArgKeyword
+  )
 }
