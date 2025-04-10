df6630476453dd76c32346d1688cf7d90e624a01
Nikhil Simha <nikhil@zipline.ai>
2025-04-10 12:42:44 -0700

    fix: compiler fixes (#619)

--- api/python/ai/chronon/repo/compilev3.py
+++ api/python/ai/chronon/repo/compilev3.py
@@ -1,9 +1,11 @@
 import os
+import sys
 
 import click
 
 from ai.chronon.cli.compile.compile_context import CompileContext
 from ai.chronon.cli.compile.compiler import Compiler
+from ai.chronon.cli.compile.display.console import console
 
 
 @click.command(name="compile")
@@ -11,8 +13,22 @@ from ai.chronon.cli.compile.compiler import Compiler
     "--chronon_root",
     envvar="CHRONON_ROOT",
     help="Path to the root chronon folder",
+    default=os.getcwd(),
 )
 def compile_v3(chronon_root):
+
+    print()
+
+    if chronon_root not in sys.path:
+        console.print(
+            f"Adding [cyan italic]{chronon_root}[/cyan italic] to python path, during compile."
+        )
+        sys.path.append(chronon_root)
+    else:
+        console.print(
+            f"[cyan italic]{chronon_root}[/cyan italic] already on python path."
+        )
+
     return __compile_v3(chronon_root)
 
 
