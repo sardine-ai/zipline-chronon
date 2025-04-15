8c3db6f9a6487001f0a9de0db05997672f03f6f6
Thomas Chow <thomas@zipline.ai>
2025-04-14 18:37:23 -0700

    chore: clean up wheel building (#640)
    ## Summary
    
    - cleanup regardless of success or failure. 
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Chores**
    - Improved the process for building and storing Python wheel files to
    enhance build isolation and cleanup. No changes to user-facing features.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":"main"}
    ```
    -->
    
    ---------
    
    Co-authored-by: Thomas Chow <thomaschow369@gmail.com>

--- scripts/distribution/build_wheel.sh
+++ scripts/distribution/build_wheel.sh
@@ -3,4 +3,8 @@ for file in api/thrift/*.thrift; do
   thrift --gen py -out api/python/ "$file"
 done
 
-VERSION=$1 pip wheel api/python
+SCRATCH_DIR=$(mktemp -d)
+trap "rm -rf ${SCRATCH_DIR}" EXIT
+ENV_VERSION=$1
+VERSION=$ENV_VERSION pip wheel api/python --wheel-dir "${SCRATCH_DIR}"
+cp ${SCRATCH_DIR}/zipline_ai-${ENV_VERSION}*.whl .
