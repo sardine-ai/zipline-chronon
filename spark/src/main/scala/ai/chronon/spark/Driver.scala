de9c7bb8831e8643043ffdfbe40bc0f17639f948
Thomas Chow <thomas@zipline.ai>
2025-04-14 19:31:52 -0700

    fix: get the full table name properly from Driver (#641)
    ## Summary
    
    - #586 did a refactor on the naming of internal tables - we need to
    update the Driver code to account for this.
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Bug Fixes**
    - Improved handling of table name parsing by changing the delimiter from
    a dot to a double underscore when extracting namespace and table name.
    - Updated the method for computing source output table names to enhance
    accuracy in table identification.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":"main"}
    ```
    -->
    
    Co-authored-by: Thomas Chow <thomaschow369@gmail.com>

--- spark/src/main/scala/ai/chronon/spark/Driver.scala
+++ spark/src/main/scala/ai/chronon/spark/Driver.scala
@@ -17,7 +17,6 @@
 package ai.chronon.spark
 
 import ai.chronon.api
-import ai.chronon.api.Constants
 import ai.chronon.api.Constants.MetadataDataset
 import ai.chronon.api.Extensions.{GroupByOps, JoinPartOps, MetadataOps, SourceOps}
 import ai.chronon.api.planner.RelevantLeftForJoinPart
@@ -28,39 +27,30 @@ import ai.chronon.online.{Api, MetadataDirWalker, MetadataEndPoint, TopicChecker
 import ai.chronon.orchestration.{JoinMergeNode, JoinPartNode}
 import ai.chronon.spark.batch._
 import ai.chronon.spark.format.Format
-import ai.chronon.spark.stats.CompareBaseJob
-import ai.chronon.spark.stats.CompareJob
-import ai.chronon.spark.stats.ConsistencyJob
-import ai.chronon.spark.stats.drift.Summarizer
-import ai.chronon.spark.stats.drift.SummaryPacker
-import ai.chronon.spark.stats.drift.SummaryUploader
+import ai.chronon.spark.stats.drift.{Summarizer, SummaryPacker, SummaryUploader}
+import ai.chronon.spark.stats.{CompareBaseJob, CompareJob, ConsistencyJob}
 import ai.chronon.spark.streaming.JoinSourceRunner
 import org.apache.commons.io.FileUtils
 import org.apache.spark.SparkFiles
 import org.apache.spark.sql.streaming.StreamingQueryListener
-import org.apache.spark.sql.streaming.StreamingQueryListener.QueryProgressEvent
-import org.apache.spark.sql.streaming.StreamingQueryListener.QueryStartedEvent
-import org.apache.spark.sql.streaming.StreamingQueryListener.QueryTerminatedEvent
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SparkSessionExtensions
+import org.apache.spark.sql.streaming.StreamingQueryListener.{
+  QueryProgressEvent,
+  QueryStartedEvent,
+  QueryTerminatedEvent
+}
+import org.apache.spark.sql.{DataFrame, SparkSession, SparkSessionExtensions}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
-import org.rogach.scallop.ScallopConf
-import org.rogach.scallop.ScallopOption
-import org.rogach.scallop.Subcommand
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand}
+import org.slf4j.{Logger, LoggerFactory}
 import org.yaml.snakeyaml.Yaml
 
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.{Files, Paths}
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.Await
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.reflect.ClassTag
 import scala.reflect.internal.util.ScalaClassLoader
 
@@ -845,7 +835,7 @@ object Driver {
       sourceWithFilterNode.setExcludeKeys(join.skewKeys)
 
       // Set the metadata
-      val sourceOutputTable = JoinUtils.computeLeftSourceTableName(join)
+      val sourceOutputTable = JoinUtils.computeFullLeftSourceTableName(join)
       println(s"Source output table: $sourceOutputTable")
 
       // Split the output table to get namespace and name
