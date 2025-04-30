a7ee7c450a13892a658d58b885311331a8b84583
ezvz <vzanoyan@gmail.com>
2025-04-29 21:08:53 -0700

    WIP

--- docker-init/demo/load_summaries.sh
+++ docker-init/demo/load_summaries.sh
@@ -1,6 +1,6 @@
 # Kick off the ObsDemo spark job in the app container
 
-docker compose -f docker-init/compose.yaml exec app /opt/spark/bin/spark-submit \
+docker-compose -f docker-init/compose.yaml exec app /opt/spark/bin/spark-submit \
   --master "local[*]" \
   --driver-memory 8g \
   --conf "spark.driver.maxResultSize=6g" \
