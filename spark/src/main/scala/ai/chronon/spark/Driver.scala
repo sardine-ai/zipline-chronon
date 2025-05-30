aea8baa8e03ef534ddd1742e19f749f5d1c9756c
Thomas Chow <thomas@zipline.ai>
2025-05-30 13:53:53 -0700

    feat: support force overwrite for stagingquery (#819)
    ## Summary
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new command-line option to allow overwriting existing
    partitions during staging query backfill jobs.
    - Enhanced processing logic to support forced overwriting of partitions
    when enabled.
    - Added logging to indicate when the overwrite option is enabled and
    which partition ranges will be processed.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":"main"}
    ```
    -->
    
    ---------
    
    Co-authored-by: thomaschow <thomaschow369@gmail.com>
    Co-authored-by: david-zlai <david@zipline.ai>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- spark/src/main/scala/ai/chronon/spark/Driver.scala
+++ spark/src/main/scala/ai/chronon/spark/Driver.scala
@@ -17,29 +17,29 @@
 package ai.chronon.spark
 
 import ai.chronon.api
-import ai.chronon.api.{Constants, DateRange, PartitionRange, ThriftJsonCodec}
 import ai.chronon.api.Constants.MetadataDataset
 import ai.chronon.api.Extensions.{GroupByOps, JoinPartOps, MetadataOps, SourceOps}
 import ai.chronon.api.planner.RelevantLeftForJoinPart
 import ai.chronon.api.thrift.TBase
-import ai.chronon.online.{Api, MetadataDirWalker, MetadataEndPoint, TopicChecker}
+import ai.chronon.api.{Constants, DateRange, PartitionRange, ThriftJsonCodec}
 import ai.chronon.online.fetcher.{ConfPathOrName, FetchContext, FetcherMain, MetadataStore}
+import ai.chronon.online.{Api, MetadataDirWalker, MetadataEndPoint, TopicChecker}
 import ai.chronon.orchestration.{JoinMergeNode, JoinPartNode}
 import ai.chronon.spark.batch._
 import ai.chronon.spark.catalog.{Format, TableUtils}
 import ai.chronon.spark.join.UnionJoin
-import ai.chronon.spark.stats.{CompareBaseJob, CompareJob, ConsistencyJob}
 import ai.chronon.spark.stats.drift.{Summarizer, SummaryPacker, SummaryUploader}
+import ai.chronon.spark.stats.{CompareBaseJob, CompareJob, ConsistencyJob}
 import ai.chronon.spark.streaming.JoinSourceRunner
 import org.apache.commons.io.FileUtils
 import org.apache.spark.SparkFiles
-import org.apache.spark.sql.{DataFrame, SparkSession, SparkSessionExtensions}
 import org.apache.spark.sql.streaming.StreamingQueryListener
 import org.apache.spark.sql.streaming.StreamingQueryListener.{
   QueryProgressEvent,
   QueryStartedEvent,
   QueryTerminatedEvent
 }
+import org.apache.spark.sql.{DataFrame, SparkSession, SparkSessionExtensions}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand}
@@ -50,8 +50,8 @@ import java.io.File
 import java.nio.file.{Files, Paths}
 import scala.collection.JavaConverters._
 import scala.collection.mutable
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 import scala.reflect.ClassTag
 import scala.reflect.internal.util.ScalaClassLoader
 
@@ -509,6 +509,12 @@ object Driver {
 
       lazy val stagingQueryConf: api.StagingQuery = parseConf[api.StagingQuery](confPath())
       override def subcommandName(): String = s"staging_query_${stagingQueryConf.metaData.name}_backfill"
+
+      val forceOverwrite =
+        opt[Boolean]("force-overwrite",
+                     descr = "Setting to true overwrites any existing partitions.",
+                     default = Some(false))
+
     }
 
     def run(args: Args): Unit = {
@@ -521,7 +527,8 @@ object Driver {
       stagingQueryJob.computeStagingQuery(args.stepDays.toOption,
                                           args.enableAutoExpand.toOption,
                                           args.startPartitionOverride.toOption,
-                                          !args.runFirstHole())
+                                          !args.runFirstHole(),
+                                          args.forceOverwrite())
 
       if (args.shouldExport()) {
         args.exportTableToLocal(args.stagingQueryConf.metaData.outputTable, tableUtils)
