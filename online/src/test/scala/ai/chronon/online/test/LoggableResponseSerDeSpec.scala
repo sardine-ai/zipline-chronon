c44c708bf426cad639fb0743f7c743de1cd34b51
Piyush Narang <piyush@zipline.ai>
2025-06-10 21:51:23 -0400

    Allow tacking on schema registry magic bytes in fetcher ooc logging (#853)
    ## Summary
    Add [schema registry magic
    bytes](https://docs.confluent.io/platform/current/schema-registry/fundamentals/serdes-develop/index.html#wire-format)
    to our kafka fetcher ooc logging. Allows customers integrated with the
    schema registry ecosystem to plug in. We don't use the schema registry
    serdes as we're using fastavro and don't want to take the potential perf
    hit.
    
    ## Checklist
    - [X] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Added support for optionally prefixing Avro-encoded Kafka messages
    with a Confluent Schema Registry wire format header when a schema
    registry ID is provided.
    - **Bug Fixes**
    - Ensured correct handling of schema registry bytes during serialization
    and deserialization of Kafka messages.
    - **Tests**
    - Introduced a new test case to verify serialization and deserialization
    with schema registry headers.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- online/src/test/scala/ai/chronon/online/test/LoggableResponseSerDeSpec.scala
+++ online/src/test/scala/ai/chronon/online/test/LoggableResponseSerDeSpec.scala
@@ -24,4 +24,25 @@ class LoggableResponseSerDeSpec extends AnyFlatSpec {
     deserializedResponse.tsMillis shouldEqual 123L
     deserializedResponse.schemaHash shouldEqual schemaHash
   }
+
+  it should "tack on schema registry bytes when schema ID is provided" in {
+    val keyBytes = "testKey".getBytes("UTF-8")
+    val valueBytes = "testValue".getBytes("UTF-8")
+    val joinName = "test.join"
+    val schemaHash = "abcd"
+    val schemaId = 42
+    val loggableResponse = LoggableResponse(keyBytes, valueBytes, joinName, 123L, schemaHash)
+    val avroBytes = LoggableResponse.toAvroBytes(loggableResponse)
+    val avroBytesWithSchemaId = LoggableResponse.prependSchemaRegistryBytes(schemaId, avroBytes)
+    avroBytesWithSchemaId should not be null
+
+    val skippedAvroBytes = avroBytesWithSchemaId.drop(5) // drop the first 5 bytes (magic byte + schema ID)
+    val deserializedResponse = LoggableResponse.fromAvroBytes(skippedAvroBytes)
+    deserializedResponse should not be null
+    deserializedResponse.keyBytes shouldEqual keyBytes
+    deserializedResponse.valueBytes shouldEqual valueBytes
+    deserializedResponse.joinName shouldEqual joinName
+    deserializedResponse.tsMillis shouldEqual 123L
+    deserializedResponse.schemaHash shouldEqual schemaHash
+  }
 }
