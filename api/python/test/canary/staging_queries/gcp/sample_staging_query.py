5f6fef0a0837d071b889182f95236c6f6a3d7a63
Thomas Chow <thomas@zipline.ai>
2025-07-25 15:04:17 -0700

    chore:clean up staging query (#999)
    ## Summary
    
    Simplify the stagingquery so we can test a controlled example of
    multi-node DAG.
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":""}
    ```
    -->
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
    * Updated configuration for environment modes, swapping and adjusting
    resource settings for "upload" and "backfill" modes.
    * Refined metadata in sample staging queries by removing specific
    time-of-day and setup parameters.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Co-authored-by: thomaschow <thomaschow369@gmail.com>

--- api/python/test/canary/staging_queries/gcp/sample_staging_query.py
+++ api/python/test/canary/staging_queries/gcp/sample_staging_query.py
@@ -13,14 +13,11 @@ WHERE ds BETWEEN '{{ start_date }}' AND '{{ end_date }}'
 v1 = StagingQuery(
     query=query,
     start_partition="2020-03-01",
-    setups=[
-        "CREATE TEMPORARY FUNCTION S2_CELL AS 'com.sample.hive.udf.S2CellId'",
-    ],
     name="sample_staging_query",
     output_namespace="sample_namespace",
     table_properties={"sample_config_json": """{"sample_key": "sample value"}"""},
     dependencies=[
-        TableDependency(table=get_join_output_table_name(training_set.v1_test, True), partition_column="ds", additional_partitions=["_HR=23:00"], offset=1)
+        TableDependency(table=get_join_output_table_name(training_set.v1_test, True), partition_column="ds", offset=1)
     ],
     version=0,
 )
