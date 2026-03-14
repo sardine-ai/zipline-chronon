509d23814f1abd627e6029c428566c06ff7ff5f0
Thomas Chow <thomas@zipline.ai>
2026-03-13 20:06:35 -0700

    chore: rename to local path (#1570)
    ## Summary
    
    ## Checklist
    - [ ] Added Unit Tests
    - [ ] Covered by existing CI
    - [ ] Integration tested
    - [ ] Documentation update
    
    
    <!-- av pr metadata
    This information is embedded by the av CLI when creating PRs to track
    the status of stacks when using Aviator. Please do not delete or edit
    this section of the PR.
    ```
    {"parent":"main","parentHead":"","trunk":"main"}
    ```
    -->
    
    Co-authored-by: thomaschow <thomaschow369@gmail.com>

--- cloud_aws/src/main/scala/ai/chronon/integrations/aws/EmrServerlessSubmitter.scala
+++ cloud_aws/src/main/scala/ai/chronon/integrations/aws/EmrServerlessSubmitter.scala
@@ -582,7 +582,12 @@ object EmrServerlessSubmitter {
         arg.substring(eqIdx + 1).split(",")
     }
 
-    val finalArgs = userArgs.toSeq
+    // Rewrite --conf-path from S3 URI to local filename since --files distributes it locally
+    val finalArgs = userArgs.map { arg =>
+      if (arg.startsWith("--conf-path=") && arg.contains("://")) {
+        s"--conf-path=${arg.split("/").last}"
+      } else arg
+    }.toSeq
     val modeConfigProperties = JobSubmitter.getModeConfigProperties(args)
 
     val region = sys.env.getOrElse("AWS_REGION", sys.env.getOrElse("AWS_DEFAULT_REGION", "us-west-2"))
